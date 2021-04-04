d10a239efac05e53a5931a7caa392750b6a5bdd7
ice812 <72642439+ice812@users.noreply.github.com>
2021-04-04 10:52:29 -0700

    rene-saletros

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,38 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components'
+
+const StarStyled = styled.div`
+.data {
+    display:inline-block;
+    justify-content: space-around;
+    color: white;
+    text-shadow: 2px 2px 5px rgb(230, 20, 20);
+    
+}
+`
+
+const data = (props) => { 
+const {data} = props 
+
+
+return  (
+    <StarStyled>
+    <div class="H1">
+        <h1>{data.name}</h1>
+     </div>
+    <div class="data"> 
+        <p>Birth year: {data.birth_year}</p>
+        <p>Eyes color: {data.eye_color}</p>
+        <p>Gender: {data.gender}</p>
+        <p>Hair Color: {data.hair_color}</p>
+        <p>Mass: {data.mass}</p>
+        <p>Skin Color: {data.skin_color}</p>
+    </div>
+    </StarStyled>
+  );
+}
+
+export default data;
+
+
